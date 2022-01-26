9e910085469cdcf2a0905acb8e7f0dc9385c9423
mayhem <mayhem@c0refailure.com>
2022-01-26 11:12:38 +1100

    Stop redundant copy, no longer publish .box

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -11,13 +11,5 @@ rm -rf packer_cache output-qemu builds
 #Do the build
 packer build packer.json
 
-# Copy output
-cp output-qemu/kali-linux-rolling ~/kali-linux-rolling
-cp builds/kali-linux-rolling.box ~/kali-linux-rolling.box
-
 # Push output to S3
-mc cp ~/kali-linux-rolling s3/pentesterwtf/qemu/kali.qcow2
-mc cp ~/kali-linux-rolling.box s3/pentesterwtf/vagrant/kali.box
-
-# Delete artefacts
-rm -f ~/kali-linux-rolling; rm -f ~/kali-linux-rolling.box
+mc cp output-qemu/kali-linux-rolling s3/pentesterwtf/qemu/kali.qcow2
